f2625addb7b459d5ffb647f36ad524901ab006b5
Azamat Kanametov <akkanametov@gmail.com>
2021-06-10 02:21:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-from setuptools import setup
+import os
+import re
+
+from setuptools import setup, find_packages
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='deepl',
+    packages=find_packages(),
     url='https://github.com/akanametov/deepl',
     author='Azamat Kanametov',
     author_email='akkanametov@gmail.com',
-    # Needed to actually package something
-    packages=['deepl'],
-    # Needed for dependencies
     install_requires=['numpy', 'scikit-learn'],
-    # *strongly* suggested for sharing
     version='0.1',
-    # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
-)
+    include_package_data=True)
